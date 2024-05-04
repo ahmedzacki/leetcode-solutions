dc6a19393c40311a58f449321e25d8e9d6d48e40
Ahmed Handulle <ahmedxandulle01@gmail.com>
2024-05-04 15:40:38 -0400

    Update solution.py

--- Stacks & Queues/71. Simplify Path/solution.py
+++ Stacks & Queues/71. Simplify Path/solution.py
@@ -1,1 +1,19 @@
 
+‌class Solution:
+    def simplifyPath(self, path: str) -> str:
+
+        data = path.split("/")
+
+        stack = []
+
+        for item in data:
+            if item == "..":
+                if stack:
+                    stack.pop()
+            elif item != " " and item != "" and item != ".":
+                stack.append(item)
+
+        if stack:
+            return "/" + "/".join(stack)
+        else:
+            return "/"
